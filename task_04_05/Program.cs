229c87d638fb7b38b8d5cd3c723224a178375109
akumilli <arinabilalova27@gmail.com>
2025-01-26 23:15:43 +0500

    5 задача 4 урока

---
+++ task_04_05/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace Task_04_05
+{
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int[] precs = new int[30];
+
+            int decada1 = 0;     
+            int decada2 = 0;     
+            int decada3 = 0;     
+            int max = 0;   
+            int day = 0; 
+
+            for (int i = 0; i < precs.Length; i++)
+            {
+                precs[i] = random.Next(0, 301);
+                  if (i < 10)
+                    decada1 += precs[i];
+                else if (i >= 20)
+                    decada3 += precs[i];
+                else
+                    decada2 += precs[i];
+                if (max < precs[i])
+                {
+                    max = precs[i];
+                    day = i;
+                }
+
+                
+                if (precs[i] == 0)
+                    Console.WriteLine($"{i + 1} день без осадков");
+            }
+
+            Console.WriteLine($"первая декада: {decada1}мм осадков, вторая: {decada2},третья: {decada3}.");
+            Console.WriteLine($"{day} числа: {max}мм - самые сильные осадки.");
+        }
+    }
+}
