644b8ce769dbad4f039a27caad7d619d2c829e99
akumilli <arinabilalova27@gmail.com>
2025-01-26 23:43:33 +0500

    8 задача 4 урока

---
+++ task_04_08/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace task_04_08
+{
+    internal class Program
+    {
+        /*Дан массив, содержащий последовательность 50 случайных чисел.
+         * Найти количество пар элементов, значениякоторых равны.*/
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int[] massiv = new int[50];    
+            for (int i = 0; i < massiv.Length; i++)
+                massiv[i] = rnd.Next(0, 10);
+            int count = 0;
+            for (int i = 1; i < massiv.Length; i++)
+                if (massiv[i - 1] == massiv[i])
+                    count++;
+
+            Console.WriteLine($" одинаковых пар элементов: {count} ");
+        }
+    }
+}
