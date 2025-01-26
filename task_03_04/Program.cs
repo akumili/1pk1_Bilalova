192289c059c7d5971406c6c475a96b38660237cd
akumilli <arinabilalova27@gmail.com>
2025-01-26 18:27:38 +0500

    выполнена 4 задача 3 урока

--- task_03_04/Program.cs
+++ task_03_04/Program.cs
@@ -1,10 +1,30 @@
-﻿namespace task_03_04
+﻿namespace Task_03_04
 {
     internal class Program
     {
+        /*
+         * Пользователь вводить в консоли произвольный текст. После каждого ввода консоль очищается. Когда
+         * пользователь вводить слово «exit» или пустую строку – ввод останавливается и выводиться количество строк
+         * введенных пользователем.
+         */
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Введите текст.");
+
+            int count = 0; 
+            while (true)
+            {
+                count++;
+                Console.Clear();
+
+                string text = Console.ReadLine();
+                if (text == "exit" || text == "")
+                {
+
+                    Console.WriteLine($"Количество введенных строк :{count}");
+                    break;
+                }
+            }
         }
     }
 }
