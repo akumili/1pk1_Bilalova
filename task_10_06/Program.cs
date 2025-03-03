e4f859c3fe8f8f92866143bc54f0bc6c3c7edca0
akumilli <arinabilalova27@gmail.com>
2025-03-04 03:18:53 +0500

    задача 6

---
+++ task_10_06/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace task_10_06
+{
+    internal class Program
+    {
+        /*Создать Метод ArrayGeneration не возвращающий значения, принимает целое число n,
+         * выводит наконсольсгенерированный массив размерности n*n.*/
+        static void Main(string[] args)
+        {
+            int n = 4; // просвоим переменной цифру 4
+            ArrayGeneration(n);
+        }
+
+        static void ArrayGeneration(int n)
+        {
+           
+            int[,] array = new int[n, n]; //  массив размером n*n
+            Random random = new Random();
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    array[i, j] = random.Next(1, 45); //заполняем рандом числами от 1 до 44
+                    Console.Write(array[i, j] + "\t");
+                }
+                Console.WriteLine(); 
+            }
+        }
+    }
+}
