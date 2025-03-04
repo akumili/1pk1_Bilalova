01ba1ec705f5d3ceff417debeaea73899d70be8c
akumilli <arinabilalova27@gmail.com>
2025-03-05 02:43:06 +0500

    задача 7

---
+++ task_11_07/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace task_11_07
+{
+    internal class Program
+    {
+        /*Передача массива по ссылке (ref): Напишите метод, который принимает массив целых
+         * чисел по ссылке и изменяет его элементы, увеличивая каждый на 1.
+         * Проверьте, изменился ли оригинальный массив вне метода.*/
+        static void Main(string[] args)
+        {
+            int[] origArray = { 44, 4, 444, };
+
+            Console.WriteLine("перед изменением:");
+            Console.WriteLine(string.Join(", ", origArray));
+
+            // Вызываем метод для изменения массива
+            IncreaseArray(ref origArray);
+
+            Console.WriteLine("Оригинальный массив после изменения:");
+            Console.WriteLine(string.Join(", ", origArray));
+        }
+      static void IncreaseArray(ref int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] += 1; // Увеличиваем каждый элемент на 1
+            }
+        }
+
+
+    }
+}
+
