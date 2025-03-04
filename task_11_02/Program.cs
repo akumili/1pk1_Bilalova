b4fe5c49c15ce16117e827f9f441f017c2bf527d
akumilli <arinabilalova27@gmail.com>
2025-03-04 21:50:11 +0500

    задача 2

---
+++ task_11_02/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace task_11_02
+{
+    internal class Program
+    {
+        /*Передача по ссылке (ref): Напишите метод, который принимает два целых
+         * числа по ссылке и меняет их местами. Проверьте, изменились ли значения переменных вне метода*/
+        static void Main(string[] args)
+        {
+            int a = 5;
+            int b = 10;
+
+            Console.WriteLine($"Значение до вызова метода:  {a},  {b}");
+
+           
+            Swap(ref a, ref b);
+
+            Console.WriteLine($"После:  {a}, {b}");
+        }
+
+       
+        static void Swap(ref int x, ref int y)
+        {
+            int temp = x; 
+            // присваиваем значения переменным
+            x = y;        
+            y = temp;     
+        }
+    }
+}
