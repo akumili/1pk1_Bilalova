54a1cfad14e6840e48893ac8a28fb9790939d023
akumilli <arinabilalova27@gmail.com>
2025-03-04 21:28:21 +0500

    задача 1

---
+++ task_11_01/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace task_11_01
+{
+    internal class Program
+    {
+        /*Передача по значению: Напишите метод, который принимает два целых числа 
+         * и меняет их местами. Проверьте, изменились ли значения переменных вне метода*/
+        static void Main(string[] args)
+        {
+            int num1 = 4;
+            int num2 = 44;
+
+            Console.WriteLine($"Значение до вызова метода: {num1},{num2}");
+
+            Swap(num1, num2);
+
+            Console.WriteLine($"Значение после: {num1},  {num2}");
+        }
+
+        static void Swap(int a, int k)
+        {
+            int f = a;
+            a = k;
+            k = f;
+
+         
+            Console.WriteLine($"Значение внутри метода:{a},{k}");
+
+        }
+    }
+}
