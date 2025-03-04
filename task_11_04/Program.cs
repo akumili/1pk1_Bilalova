9b4997c4fff3c72b7c7046d604574196567a5902
akumilli <arinabilalova27@gmail.com>
2025-03-05 02:14:48 +0500

    задача 4

---
+++ task_11_04/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace task_11_04
+{
+    internal class Program
+    {
+        /*Массив параметров (params): Напишите метод, который принимает массив чисел и возвращает
+         * их среднее значение. Используйте ключевое слово params*/
+        static void Main(string[] args)
+        {
+            double av1 = CalculateAverage(4, 44, 444);
+            Console.WriteLine($"Сред.знач {av1}");
+
+            double av2 = CalculateAverage(10, 40, 400);
+            Console.WriteLine($"Сред.знач: {av2}");
+
+          
+        }
+        static double CalculateAverage(params int[] numbers)
+        {
+            if (numbers.Length == 0)
+            {
+                return 0;
+            }
+
+            int sum = 0;
+
+            // Суммa
+            foreach (int number in numbers)
+            {
+                sum += number;
+            }
+            return (double)sum / numbers.Length;
+        }
+    }
+}
