3536310a5567304466b835d587f63bad6409da09
akumilli <arinabilalova27@gmail.com>
2025-04-08 04:09:03 +0500

    задача3

--- task_20_03/Program.cs
+++ task_20_03/Program.cs
@@ -8,32 +8,47 @@
 • Cancelled (отменён)
 Напишите метод, который меняет статус заказа. Если заказ уже доставлен или отменён, запретите изменение статуса. Выводите сообщение при каждом изменении статуса (например,
 "Заказ переведён в статус: Отправлен"*/
+
+    public enum OrderStatus
+    {
+        New,
+        Processing,
+        Shipped,
+        Delivered,
+        Cancelled
+    }
+
     internal class Program
     {
-        static void Main(string[] args)
-        {
-                   int orderId = 1;
-                int orderStatus = 1;
-                OrderStatus status = OrderStatus.New;
+       
+      
+            static void Main(string[] args)
+            {
+                int orderId = 1;
+                OrderStatus status = OrderStatus.New; 
 
-              
+                // фигурируем изменение статуса
                 void ChangeStatus(OrderStatus newStatus)
                 {
-                   
+                    if (status == OrderStatus.Delivered || status == OrderStatus.Cancelled)
+                    {
+                        Console.WriteLine($"Невозможно сменить статус заказа {orderId}, так как заказ{status}.");
+                        return;
+                    }
 
-                    
-                    status = newStatus;
-                    Console.WriteLine($"Заказ {orderId} переведён в статус: {status}");
+                    // смена статуса 
+                    Console.WriteLine($"Заказ {orderId} теперь находится в статусе: {status}");
                 }
-
-           
                 ChangeStatus(OrderStatus.Processing);
                 ChangeStatus(OrderStatus.Shipped);
                 ChangeStatus(OrderStatus.Delivered);
                 ChangeStatus(OrderStatus.Cancelled);
-                ChangeStatus(OrderStatus.New); 
+
+
             }
-        }
+           
     }
 }
+
+
 
